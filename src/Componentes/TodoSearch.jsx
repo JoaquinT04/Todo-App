2800a3d5ad87fd0014bf4facbd52c52328cf2476
Joaquin Tejerina <joaquintejerina19@hotmail.com>
2021-12-13 20:41:03 -0300

    Manejo del estado

--- src/Componentes/TodoSearch.jsx
+++ src/Componentes/TodoSearch.jsx
@@ -1,20 +1,52 @@
 import React from "react";
 import "../Css/TodoSearch.css"
 
-function TodoSearch(){
-	const onSearchValueChange = (event) => {
-		console.log(event)
-		console.log(event.target.value)
+/* Antes 
+class Componente extends React.Component{
+	constructor() {
+		this.state = {
+			patito:"joaquin"
+		};
 	}
 
-	return(
+	render(){
+		return(
+			<div onClick={() => this.setState("Enrique")}>{this.state.patito}</div>
+		)
+	}
+}
+*/
+function TodoSearch(){
+	// const [patito, setPatito] = React.useState("Joaquin");
+	const [searchValue, setSearchValue] = React.useState('');
+
+	const onSearchValueChange = (event) => {
+		// console.log(event)
+		console.log(event.target.value)
+		setSearchValue(event.target.value)
+	}
+
+	// Se puede retornar un array
+/*	return[
 		<input 
 			className="TodoSearch" 
 			type="text" 
 			placeholder="Cebolla" 
+			onChange={() => setPatito("Enrique")}
+		/>,
+		<p>{patito}</p>
+	];*/
+
+	return[
+		<input 
+			className="TodoSearch" 
+			type="text" 
+			placeholder="Cebolla" 
+			value={searchValue}
 			onChange={onSearchValueChange}
-		/>
-	);
+		/>,
+		<p>{searchValue}</p>
+	]
 }
 
 export { TodoSearch };
